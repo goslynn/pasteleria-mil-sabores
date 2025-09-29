4c316114922f11f17c6c9bf49976c3617a93bd58
goslynn <vct.gonzaleza@gmail.com>
2025-09-29 16:59:36 -0300

    fix a tipado de layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -31,7 +31,7 @@ export const viewport: Viewport = {
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
-    children: React.JSX.Element;
+    children: React.ReactNode;
 }>) {
     return (
         <html lang="es" suppressHydrationWarning>
