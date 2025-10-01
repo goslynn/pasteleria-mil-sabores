272062d327e3820dcbaa44c2c7e6aaa3c66a08a4
goslynn <vct.gonzaleza@gmail.com>
2025-10-01 00:08:12 -0300

    docker anda perfectamente pero sigo sin saber porque estoy atrapado en loop infinito del login

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,12 +1,22 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    {
+        name: 'strapi::session',
+        config: {
+            key: 'sid',
+            httpOnly: true,   // sigue siendo buena práctica
+            secure: false,    // <- permite cookies por HTTP (sin "Secure")
+            sameSite: 'lax',  // evita problemas típicos; usa 'strict' si quieres
+            rolling: true,
+            renew: true,
+        },
+    },
+    'strapi::favicon',
+    'strapi::public',
 ];
